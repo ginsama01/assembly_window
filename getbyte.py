1c4eef87b3cb46f7f4a423e372785e322e58304f
ginsama01 <ginsama2001@gmail.com>
2022-06-27 00:04:00 +0700

    update

--- getbyte.py
+++ getbyte.py
@@ -1,16 +1,67 @@
-f = open("byte.txt", "rb")
-s = f.read()
-f = open("hexa", "rb")
-g = f.read()
+hex1 = '''1, 0, 0CCCCCC74, 1, 1, 0CCCCCC48, 1, 2, 0CCCCCC3B, 2, 88, 0CCCC2481
+3, 6F, 6F593363, 2, 84, 0CCCC0101, 2, 0A, 0CCCCAF35, 1, 0D0
+0CCCCCC33, 3, 0F, 76784D64, 4, 12, 1AB120DD, 1, 106, 0CCCCCC0C
+3, 0E8, 3542446A, 2, 1D, 0CCCC21A6, 2, 1F, 0CCCC8ABE, 1, 21
+0CCCCCC4C, 2, 22, 0CCCC0E26, 1, 24, 0CCCCCC35, 1, 5D, 0CCCCCC3B
+3, 2B, 7539456A, 4, 16, 0DED3F88, 3, 0EB, 7A56336A, 2, 32, 0CCCCAF35
+2, 30, 0CCCC2FAB, 2, 8, 0CCCC3681, 3, 34, 75636C6A, 1, 37, 0CCCCCC00
+2, 38, 0CCCC3C25, 3, 53, 67524649, 1, 3E, 0CCCCCC21, 1, 3F, 0CCCCCC54
+1, 92, 0CCCCCC37, 1, 86, 0CCCCCC74, 4, 109, 13FD36C0, 1, 4A
+0CCCCCC00, 1, 10D, 0CCCCCC52, 4, 4D, 43BA3DC1, 3, 11C, 6F773264
+4, 3A, 0CB073CD, 4, 56, 6B973CD, 3, 5A, 796D3251, 4, 10F, 10AE36CB
+1, 5E, 0CCCCCC3D, 2, 5F, 0CCCC2101, 4, 68, 6B52788, 1, 0BA, 0CCCCCC44
+4, 9A, 6BA3ADC, 3, 6C, 6D4A4649, 4, 4, 43AE6288, 4, 72, 0EFD20C1
+2, 0C7, 0CCCC863D, 1, 7A, 0CCCCCC2B, 4, 7B, 0DB82788, 1, 0C6
+0CCCCCC0E, 1, 83, 0CCCCCC2B, 1, 3, 0CCCCCC01, 4, 93, 13BC2388
+4, 0A9, 43B23788, 3, 0C, 436D475A, 4, 8A, 11BC36CE, 4, 8E, 11BC73DB
+4, 0AF, 0AB527D1, 2, 44, 0CCCCA525, 1, 97, 0CCCCCC37, 2, 98
+0CCCC2F01, 4, 61, 2FD3CDC, 1, 9E, 0CCCCCC52, 4, 9F, 3AFD7DDB
+4, 46, 17B126CB, 1, 0A7, 0CCCCCC33, 1, 2F, 0CCCCCC00, 1, 87
+0CCCCCC48, 1, 0AD, 0CCCCCC33, 1, 0AE, 0CCCCCC4E, 4, 0C9, 0FD26C7
+2, 0B3, 0CCCCA122, 1, 0B5, 0CCCCCC00, 4, 0B6, 43A83CD1, 3, 65
+73593351, 1, 0BB, 0CCCCCC37, 1, 0BC, 0CCCCCC31, 4, 0D2, 0CA973DC
+3, 0BF, 3842545A, 2, 0C2, 0CCCC2181, 1, 0DA, 0CCCCCC4E, 4, 7F
+17B430C9, 4, 76, 0FB821CD, 4, 0FB, 11AD73CD, 2, 0CD, 0CCCC26A6
+1, 0CF, 0CCCCCC00, 2, 4B, 0CCCC2C25, 1, 0D1, 0CCCCCC31, 2, 0BD
+0CCCC22A3, 3, 113, 796D4749, 2, 0C4, 0CCCCA426, 3, 0EF, 6C6D476A
+4, 0DC, 0DBC73CD, 1, 0EE, 0CCCCCC00, 4, 0E4, 0CAF27C6, 3, 1A
+5539315A, 1, 2E, 0CCCCCC35, 4, 0E0, 0CBE73CC, 1, 0DB, 0CCCCCC35
+2, 0F2, 0CCCCA48C, 4, 0F4, 7B33288, 2, 25, 0CCCC39A7, 4, 40
+5B43788, 1, 0FF, 0CCCCCC3D, 3, 100, 6B563251, 3, 103, 6D4A5864
+4, 27, 3CBA1DC7, 2, 107, 0CCCC062B, 4, 0A3, 0FD26C7, 1, 0A8
+0CCCCCC4E, 1, 10E, 0CCCCCC3D, 3, 0F8, 67524649, 4, 0D6, 2B53088
+2, 116, 0CCCC8625, 4, 118, 0CFD20DC, 2, 51, 0CCCCA2A8, 3, 11F
+33566C63, 3, 122, 6B4A5851, 1, 125, 0CCCCCC0E'''.replace("\n", ", ")
+
+hex2 = '''73, 0E9, 39, 0D0, 98, 0BB, 0D6, 23, 16, 19, 0FC, 7C, 0F
+32, 80, 0B2, 9C, 57, 36, 9E, 91, 4D, 0DF, 7A, 8, 42, 76
+0A5, 11, 0AD, 3E, 0D2, 65, 4F, 71, 20, 0A0, 28, 0C3, 33
+4E, 6C, 79, 95, 0AF, 6B, 0C8, 70, 0A2, 41, 92, 0BA, 4B
+0D1, 0E3, 0BC, 2B, 0F4, 1C, 46, 78, 0D9, 0B6, 4, 0ED, 96
+68, 97, 0F5, 9, 3A, 25, 0EB, 0BE, 49, 0D8, 6D, 0B5, 13
+7E, 0, 77, 6F, 0B4, 0E, 1D, 0B7, 2C, 0CA, 7F, 3C, 5F, 7D
+0A9, 88, 0C4, 0C0, 5E, 18, 0CD, 0E0, 0C, 62, 29, 54, 84
+7, 47, 0C9, 0F7, 2E, 6, 0E2, 24, 83, 0E4, 52, 15, 45, 43
+0DA, 31, 82, 87, 0B8, 14, 0E7, 0CF, 0E5, 40, 1A, 0DD, 9A
+35, 85, 0F3, 63, 0B1, 0F0, 3D, 0D, 0EA, 8B, 0EE, 99, 0AE
+0A4, 51, 0A8, 1E, 1B, 0C5, 34, 4C, 0FD, 0FF, 0EC, 37, 64
+75, 5, 1, 8C, 21, 0A3, 60, 50, 6A, 0B9, 5C, 53, 0CE, 26
+0C1, 3B, 0F2, 3F, 66, 0CC, 2F, 0A1, 94, 56, 59, 4A, 9F
+0D7, 89, 48, 5B, 12, 9D, 8F, 55, 0D5, 0BF, 5D, 2D, 0F8
+1F, 30, 0B, 5A, 44, 67, 2A, 38, 0F9, 0F6, 6E, 7B, 0EF
+0E8, 8A, 0DE, 0C7, 0F1, 0A7, 0CB, 0DC, 0D4, 0D3, 27, 0FE
+10, 2, 0BD, 90, 0FA, 0E1, 69, 0E6, 72, 0AB, 0AC, 22, 8E
+86, 9B, 0FB, 0A6, 17, 0B3, 61, 74, 0C6, 0C2, 58, 0B0, 0AA
+0DB, 93, 8D, 3, 0A, 81, 0, 0, 8A, 1, 0, 0, 89, 1, 75'''.replace("\n", ", ")
 key = ""
-for i in range(298):
+for i in range(294):
     key = key + "x"
 key = list(key)
 
 
 
 def function1(j, num):
-    output = num
+    output = num & 0xff
     if output % 2 == 0:
         output ^= 32
     else:
@@ -18,6 +69,7 @@ def function1(j, num):
     key[j] = chr(output)
 
 def function2(j, num):
+    num &= 0xffff
     for k in [5, 4, 3, 2, 1]:
         num ^= 0x1693
         num = ((num << (16 - k)) | (num >> k)) & 0xffff
@@ -46,19 +98,22 @@ def function3(j, num):
 
 def function4(j, num):
     a1 = []
-    for i in g:
-        a1.append(i)
-        print(i)
+    for i in hex2.split(", "):
+        a1.append(int(i, 16))
     v7 = 0
     v6 = 0
     v5 = 0
     v4 = 4
     while v5 < v4:
         v7 = (v7 + 1) % 256
-        v6 = (v6 + a1[v7 * 2]) % 256
-        v8 = a1[v7 * 2]
-        a1[v7 * 2] = a1[v6 * 2]
-        a1[v6 * 2] = v8
+        v6 = (v6 + a1[v7]) % 256
+        v8 = a1[v7]
+        a1[v7] = a1[v6]
+        a1[v6] = v8
+        gt1 = (num >> (8 * v5)) & 0xff
+        gt2 = a1[(a1[v6] + a1[v7]) % 256]
+        key[j + v7 - 1] = chr(gt1 ^ gt2)
+        v5 += 1
         
 
 
@@ -66,47 +121,58 @@ count = 0
 fnc = 0
 j = 0
 num = 0
-c1 = 0
-c2 = 0
-c3 = 0
-c4 = 0
 
-for i in s:
-    if i == 32:
-        i = 0
+for i in hex1.split(", "):
     if count == 0:
-        fnc = i 
-    if count == 4:
-        j = i 
-    if count == 5:
-        j = (i << 8) + j
-    if count == 8:
-        num = i
-    if count == 9 and fnc >= 2:
-        num = (i << 8) + num  
-    if count == 10 and fnc >= 3:
-        num = (i << 16) + num
-    if count == 11 and fnc >= 3:
-        num = (i << 24) + num
-    count = count + 1
-    if count == 12:
+        fnc = int(i, 16)
+    if count == 1:
+        j = int(i, 16)
+    if count == 2:
+        num = int(i, 16)
+    count += 1
+    if count == 3:
         if fnc == 1:
             function1(j, num)
-            c1+=1
         if fnc == 2:
             function2(j, num)
-            c2+=1
         if fnc == 3:
             function3(j, num)
-            c3+=1
         if fnc == 4:
-            c4+=1
+            function4(j, num)
         count = 0
+    
+# for i in s:
+#     if i == 32:
+#         i = 0
+#     if count == 0:
+#         fnc = i 
+#     if count == 4:
+#         j = i 
+#     if count == 5:
+#         j = (i << 8) + j
+#     if count == 8:
+#         num = i
+#     if count == 9 and fnc >= 2:
+#         num = (i << 8) + num  
+#     if count == 10 and fnc >= 3:
+#         num = (i << 16) + num
+#     if count == 11 and fnc >= 3:
+#         num = (i << 24) + num
+#     count = count + 1
+#     if count == 12:
+        # if fnc == 1:
+        #     function1(j, num)
+        #     c1+=1
+        # if fnc == 2:
+        #     function2(j, num)
+        #     c2+=1
+        # if fnc == 3:
+        #     function3(j, num)
+        #     c3+=1
+        # if fnc == 4:
+        #     function4(j, num)
+        #     c4+=1
+        #     break
+        # count = 0
 
 print(''.join(key))
-# for i in range(len(key)):
-#      print(str(i) + ":" + str(ord(key[i])))
-print(c1)
-print(c2)
-print(c3)
-print(c4)
